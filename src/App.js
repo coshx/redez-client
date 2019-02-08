569454049a0c7154f0963d1e55a85cf1b77249c0
Lane Spangler <lspangler@protonmail.com>
2019-02-08 16:57:48 -0600

    Initial tree viewer implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import './styles/TreeViewer.css';
+
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider, Query } from "react-apollo";
+
+import gql from 'graphql-tag';
+import Collapsible from 'react-collapsible';
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/"
+});
+
+const renderComponentTrees = trees => {
+  if (!trees) return;
+  return trees.map(({ path, name, children }) => (
+    <div key={path}>
+      <Collapsible
+        trigger={<div className={"TreeViewer-collapsible-trigger"}>
+                   <span>{name}</span>
+                   <span>
+                     <div className="TreeViewer-collapsible-trigger-icon">
+                     </div>
+                   </span>
+                 </div>}
+        transitionTime={200}
+        className="TreeViewer-collapsible"
+        openedClassName="TreeViewer-collapsible"
+        contentInnerClassName="TreeViewer-collapsible-content"
+      >
+        <div className='TreeViewer-children'>
+          {renderComponentTrees(children)}
+        </div>
+      </Collapsible>
+    </div>
+  ))
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ApolloProvider client={client}>
+          <div className="TreeViewer">
+            <Query query={gql`
+              {
+                generateComponentTrees {
+                  data
+                }
+              }
+            `}>
+              {({ loading, error, data }) => {
+                if (loading) return <p>Generating component tree...</p>
+                if (error) {
+                  console.error(error);
+                  return <p>An error occurred when generating the component tree.</p>
+                }
+
+                return renderComponentTrees(data.generateComponentTrees.map(tree => JSON.parse(tree.data)));
+              }}
+            </Query>
+          </div>
+        </ApolloProvider>
       </div>
     );
   }
